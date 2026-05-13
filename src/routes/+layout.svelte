2e4d89cc302f7ab53045669002c479e01177f306
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2026-05-13 09:38:09 -0700

    added preview toolbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,6 +47,7 @@
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+	<script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=revogen"></script>
 </svelte:head>
 
 <style>
